c21292f2db6cc9a47fe75417f65b48b352e2e4b5
stellar910412 <stellarlele@163.com>
2016-01-12 17:59:35 +0800

    sql

--- _build/pvuv-data.sql
+++ _build/pvuv-data.sql
@@ -10,28 +10,29 @@ Target Server Type    : MYSQL
 Target Server Version : 50087
 File Encoding         : 65001
 
-Date: 2016-01-12 11:09:50
+Date: 2016-01-12 16:02:18
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for pvuv_data
+-- Table structure for pvuv-data
 -- ----------------------------
-DROP TABLE IF EXISTS `pvuv_data`;
-CREATE TABLE `pvuv_data` (
+DROP TABLE IF EXISTS `pvuv-data`;
+CREATE TABLE `pvuv-data` (
   `id` bigint(20) NOT NULL auto_increment,
   `mac` varchar(64) NOT NULL default '0',
   `time` varchar(64) NOT NULL default '0',
   `pv` varchar(64) NOT NULL default '0',
   `download_app_times` varchar(64) NOT NULL default '0',
   `uv` varchar(64) NOT NULL default '0',
-  `android` varchar(64) NOT NULL default '0',
-  `ios` varchar(64) NOT NULL default '0',
-  `windows` varchar(64) NOT NULL default '0',
+  `uv_android` varchar(64) NOT NULL default '0',
+  `uv_ios` varchar(64) NOT NULL default '0',
+  `uv_windows` varchar(64) NOT NULL default '0',
+  `uv_others` varchar(64) NOT NULL default '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of pvuv_data
+-- Records of pvuv-data
 -- ----------------------------
