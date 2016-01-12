4011e390da8832159f0c597ed154f12a9a471690
wangmingguang <wangmingguang>
2016-01-12 18:59:19 +0800

    min.css to .css

--- application/views/pvuv.php
+++ application/views/pvuv.php
@@ -4,7 +4,7 @@
         <title>数据</title>
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link href='<?php echo base_url(); ?>application/views/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='<?php echo base_url(); ?>application/views/bootstrap/css/bootstrap.css' rel='stylesheet'>
     </head>
 	<body>
         <div id="container">
