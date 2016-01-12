027c9d07e8af02879f2a7013463cf26caba009ac
wangmingguang <wangmingguang>
2016-01-12 18:03:41 +0800

    PvUvshow

--- application/controllers/Pvuv.php
+++ application/controllers/Pvuv.php
@@ -1,26 +1,20 @@
 <?php
 class Pvuv extends CI_Controller {
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct(); 
         $this->load->model('pvuv_model');
+        $this->load->helper('url');
     }
     
-    public function index()
-    {
-        $yesterday_date = date('m-d',strtotime("-1 day"));
-        $data['mac0000'] = $this->pvuv_model->get_data_mac0000($yesterday_date,"0000"); 
-        $data['mac0020'] = $this->pvuv_model->get_data_mac0020($yesterday_date,"0020");
-        $data['mac0040'] = $this->pvuv_model->get_data_mac0040($yesterday_date,"0040");
-        $data['mac0060'] = $this->pvuv_model->get_data_mac0060($yesterday_date,"0060");
-        $data['mac0080'] = $this->pvuv_model->get_data_mac0080($yesterday_date,"0080");
-        $data['mac0100'] = $this->pvuv_model->get_data_mac0100($yesterday_date,"0100");
-        $data['mac00A0'] = $this->pvuv_model->get_data_mac00A0($yesterday_date,"00A0");
-        $data['mac00C0'] = $this->pvuv_model->get_data_mac00C0($yesterday_date,"00C0");
-        $data['mac00E0'] = $this->pvuv_model->get_data_mac00E0($yesterday_date,"00E0");
-        $data['mac0120'] = $this->pvuv_model->get_data_mac0120($yesterday_date,"0120");
-//        $sql = $this->pvuv_model->get_data($yesterday_date,"0000");
-//        echo $sql;
+    public function index(){
+        if($this->uri->segment(3) != ""){
+            $time = $this->uri->segment(3);
+        }else{
+            date_default_timezone_set('PRC');
+            $time = date('m-d');
+        }
+        $data['pvuv_data'] = $this->pvuv_model->pvuv_data($time); 
+
         $this->load->view('pvuv',$data);
     }
 }
--- application/views/pvuv.php
+++ application/views/pvuv.php
@@ -1,218 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
-<title>数据</title>
-</head>
-
+<!DOCTYPE htm>
+<html>
+    <head>
+        <title>数据</title>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link href='<?php echo base_url(); ?>/application/views/bootstrap/css/bootstrap.css' rel='stylesheet'>
+    </head>
 	<body>
-		<table border="1" style="font-family:'宋体'; font-size:30px;" class="" id="table2">
-		<tbody>
-			<tr>
-				<th>PV Total</th>
-				<th>UV Total</th>
-				<th>PV Today</th>
-				<th>UV Today</th>
-				<th>PV Yesterday</th>
-				<th>UV Yesterday</th>
-			</tr>
-			<tr>
-				<td>639757</td>
-				<td>3387</td>
-				<td>639757</td>
-				<td>3387</td>
-				<td>0</td>
-				<td>0</td>
-			</tbody>
-		</table>
-		<hr>
-		<table border="1" style="font-family:'宋体'; font-size:30px;" class="" id="table">
-        	<tbody>
-            	<tr> 
-                    <th><i class=""></i>mac</th>
-					<th><i class=""></i>time</th>
-					<th><i class=""></i>pv</th>
-					<th><i class=""></i>download_app_times</th>
-					<th><i class=""></i>uv</th>
-					<th><i class=""></i>andriod</th>
-					<th>ios</th>
-					<th>windows</th>
-					<th>unkown</th>
-					<th>others</th>
-					<th>更多操作</th>
-                 </tr>
-                <?php foreach($mac0000 as $row):?>
-				 <tr>
-                 	<td>00-00</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>             
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0000time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr> 
-                <?php endforeach?>
-                <?php foreach($mac0020 as $row):?>
-				 <tr>
-                 	<td>00-20</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>          
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0020time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-                <?php foreach($mac0040 as $row):?>
-				 <tr>
-                 	<td>00-40</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>         
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0040time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac0060 as $row):?>
-				 <tr>
-                 	<td>00-60</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>          
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0060time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac0080 as $row):?>
-				 <tr>
-                 	<td>00-80</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>            
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0080time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac0100 as $row):?>
-				 <tr>
-                 	<td>01-00</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>            
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0100time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac00A0 as $row):?>
-				 <tr>
-                 	<td>00-A0</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>              
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac00A0time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac00C0 as $row):?>
-				 <tr>
-                 	<td>00-C0</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>             
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac00C0time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac00E0 as $row):?>
-				 <tr>
-                 	<td>00-E0</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php  echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>              
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac00E0time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-				</tr>
-                <?php endforeach?>
-				<?php foreach($mac0120 as $row):?>
-				 <tr>
-                 	<td>01-20</td>
-                    <td><?php echo $row['time'];?></td>
-                    <td><?php echo $row['pv'];?></td>
-                    <td><?php echo $row['download_app_times'];?></td>
-                    <td><?php echo $row['uv'];?></td>
-                    <td><?php echo $row['android'];?></td>
-                    <td><?php echo $row['ios'];?></td>
-                    <td><?php echo $row['windows'];?></td>
-                    <td><?php echo $row['unknown'];?></td>
-                    <td><?php echo $row['others'];?></td>             
-                    <td>
-						<a class="btn btn-primary" href="http://120.26.47.127:8090/mac0120time0107.html#os">更多<i class="icon_plus_alt2"></i></a>
-					</td>
-                </tr>
-                <?php endforeach?>
-            </tbody>
-	</table>
-	<hr>
-</body>
+        <div id="container">
+            <div class="row">
+                <div class="col-md-3">PV/UV</div>
+                <div class="col-md-3"><?php date_default_timezone_set('PRC');$time = date('m-d'); echo "<a href='/Pvuv/index/".$time."'>";?>今天</a></div>
+                <div class="col-md-3"><?php $time = date('m-d',strtotime("-1 day")); echo "<a href='/Pvuv/index/".$time."'>";?>昨天</a></div>
+            </div>
+            <div class="row">
+                        <div class="col-md-1">mac</div>
+                        <div class="col-md-1">time</div>
+                        <div class="col-md-1">pv</div>
+                        <div class="col-md-2">download_app_times</div>
+                        <div class="col-md-1">uv</div>
+                        <div class="col-md-1">uv_andriod</div>
+                        <div class="col-md-1">uv_ios</div>
+                        <div class="col-md-1">uv_windows</div>
+                        <div class="col-md-1">uv_others</div>
+            </div>
+            <div class="row">
+                    <?php foreach($pvuv_data as $row):?>
+                        <div class="col-md-1"><?php echo $row['mac'];?></div>
+                        <div class="col-md-1"><?php echo $row['time'];?></div>
+                        <div class="col-md-1"><?php echo $row['pv'];?></div>
+                        <div class="col-md-2"><?php echo $row['download_app_times'];?></div>
+                        <div class="col-md-1"><?php echo $row['uv'];?></div>
+                        <div class="col-md-1"><?php echo $row['uv_android'];?></div>
+                        <div class="col-md-1"><?php echo $row['uv_ios'];?></div>
+                        <div class="col-md-1"><?php echo $row['uv_windows'];?></div>
+                        <div class="col-md-1"><?php echo $row['uv_others'];?></div>             
+                        <div class="col-md-2"></div>
+                    <?php endforeach?>
+            </div>
+        </div>
+    </body>
+    <script src='<?php echo base_url(); ?>/application/views/bootstrap/js/bootstrap.js'></script>
 </html>
