e962f9e8a5b86e16efe0557f58bde87e0ef3d99c
wangmingguang <wangmingguang>
2016-01-13 10:03:02 +0800

    update url

--- application/views/pvuv.php
+++ application/views/pvuv.php
@@ -4,15 +4,17 @@
         <title>数据</title>
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link href='<?php echo base_url(); ?>application/views/bootstrap/css/bootstrap.css' rel='stylesheet'>
+        <link href='application/views/bootstrap/css/bootstrap.css' rel='stylesheet'>
     </head>
 	<body>
         <div id="container">
             <div class="row">
                 <div class="col-md-3">PV/UV</div>
                 <div class="col-md-3"><a href="/Pvuv">所有</a></div>
-                <div class="col-md-3"><?php date_default_timezone_set('PRC');$time = date('m-d'); echo "<a href='/Pvuv/index/".$time."'>";?>今天</a></div>
-                <div class="col-md-3"><?php $time = date('m-d',strtotime("-1 day")); echo "<a href='/Pvuv/index/".$time."'>";?>昨天</a></div>
+                <div class="col-md-3"><?php date_default_timezone_set('PRC');$time = date('m-d');?>
+                <a href="<?php echo base_url('Pvuv/index')."/".$time;?>">今天</a></div>
+                <div class="col-md-3"><?php $time = date('m-d',strtotime("-1 day"));?>
+                <a href="<?php echo base_url('Pvuv/index')."/".$time;?>">昨天</a></div>
             </div>
             <div class="row">
                         <div class="col-md-1">mac</div>
@@ -43,5 +45,5 @@
             </div>
         </div>
     </body>
-    <script src='<?php echo base_url(); ?>application/views/bootstrap/js/bootstrap.js'></script>
+    <script src='application/views/bootstrap/js/bootstrap.js'></script>
 </html>
